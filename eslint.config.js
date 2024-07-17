89952472b15d8da4168a7fd76a95124372141447
Raiane Oliveira <raiane.oliveira404@gmail.com>
2024-07-17 11:19:47 -0300

    :tada Initial commit

---
+++ eslint.config.js
@@ -0,0 +1,19 @@
+import globals from "globals"
+import pluginJs from "@eslint/js"
+import tseslint from "typescript-eslint"
+import stylistic from "@stylistic/eslint-plugin"
+
+export default [
+  { files: ["**/*.{js,mjs,cjs,ts}"] },
+  { languageOptions: { globals: globals.browser } },
+  pluginJs.configs.recommended,
+  ...tseslint.configs.recommended,
+  stylistic.configs.customize({
+    "indent": 2,
+    "quotes": "double",
+    "semi": false,
+    "no-unused-vars": "warn",
+    "no-undef": "warn",
+    "skipBlankLines": true,
+  }),
+]
